212bf30f8f8c1a366852a9d3a837386ab7ed341e
thampinox <dmytro.pryshlyak@gmail.com>
2019-02-27 00:07:31 +0200

    added note for code block

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ class App extends HTMLElement {
 
         let tmpl = document.createElement('template');
         tmpl.innerHTML = template;
+
+        // Note: In the above snippet we use a template element to clone DOM, 
+        // instead of setting the innerHTML of the shadowRoot. 
+        // This technique cuts down on HTML parse costs because the content of the template 
+        // is only parsed once, whereas calling innerHTML on the shadowRoot will parse the HTML 
+        // for each instance.
+
         const shadowRoot = this.attachShadow({ mode: 'open' });
         shadowRoot.appendChild(tmpl.content.cloneNode(true));
     }
